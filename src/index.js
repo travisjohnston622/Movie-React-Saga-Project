ad958fba9d8ed3c038caa68c52c30e948f9384b0
Travis Johnston <travisjohnston4588@icloud.com>
2020-01-06 18:59:24 -0600

    moved sagas and reducers index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,49 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 // Provider allows us to use redux within our react app
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 // Import saga middleware
 import createSagaMiddleware from 'redux-saga';
-import rootReducer from './redux/reducers/RootReducer/rootReducer';
-import rootSaga from './redux/sagas/RootSaga/rootSaga';
+import { put, takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
+
+
+//sagas
+function* rootSaga() {
+    yield takeLatest('GET_MOVIES', getMovies);
+}
+
+function* getMovies() {
+    console.log('in function* getMovies');
+    try {
+        const response = yield axios({
+            method: 'GET',
+            url: '/api/movies'
+        });
+        yield put({
+            type: 'SET_MOVIES',
+            payload: response.data
+        });
+    } catch (err) {
+        console.log('error fetching your movies:', err);
+    }
+}
+
+
+//Reducers
+    const movieListReducer = (state = [], action) => {
+        switch (action.type) {
+            case 'SET_MOVIES':
+                return action.payload;
+            default:
+                return state;
+        }
+    }
+
+
 
 
 // Create sagaMiddleware
@@ -22,13 +57,16 @@ const middlewareList = process.env.NODE_ENV === 'development' ?
     [sagaMiddleware];
 
 // Create one store that all components can use
-const store = createStore(
-    rootReducer,
+const storeInstance = createStore(
+    combineReducers({
+    movieListReducer,
+    }),
     applyMiddleware(...middlewareList),
 )
 
 // Pass rootSaga into our sagaMiddleware
 sagaMiddleware.run(rootSaga);
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('react-root'));
+    ReactDOM.render(<Provider store={storeInstance}><App /></Provider>,
+        document.getElementById('root'));
 registerServiceWorker();
